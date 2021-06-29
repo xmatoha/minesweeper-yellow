6757df1bea7066b509badc4f9354596667e59de1
martin hablak <martin.hablak@sk.ibm.com>
2021-06-29 08:20:34 +0200

    US1.UAT2 GREEN

--- src/minesweeper_yellow/core.clj
+++ src/minesweeper_yellow/core.clj
@@ -1,6 +1,14 @@
 (ns minesweeper-yellow.core)
 
 (defn empty-board [n]
-  (repeat n (repeat n 0)))
+  (vec (repeat n (vec (repeat n 0)))))
 
-(defn spread-mines [board mines-count])
+(defn spread-mines [board mines-count]
+  (loop [board board]
+    (let [new-board (assoc-in
+                     board
+                     [(rand-int (count board)) (rand-int (count (get board 0)))]                      "b")
+          new-mines-count (count (filter (fn [e] (= e "b")) (flatten new-board)))]
+      (if (=  new-mines-count mines-count) new-board
+          (recur new-board)))))
+
--- test/minesweeper_yellow/core_test.clj
+++ test/minesweeper_yellow/core_test.clj
@@ -7,4 +7,4 @@
       (is (= [[0 0 0] [0 0 0] [0 0 0]] (empty-board 3))))
 
     (testing "given board of size n and number of mines m, spread mines on board randomly so that there is m mines at the end"
-      (is (= 3 (filter (fn [e] (= e "b")) (flatten (spread-mines (empty-board 3) 3))))))))
+      (is (= 3 (count (filter (fn [e] (= e "b")) (flatten (spread-mines (empty-board 3) 3)))))))))
