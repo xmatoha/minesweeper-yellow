6c14c385224e10ebe48d23a650b5b1dc3e97c9db
martin hablak <martin.hablak@sk.ibm.com>
2021-06-29 07:59:08 +0200

    US1.UAT2 RED

--- src/minesweeper_yellow/core.clj
+++ src/minesweeper_yellow/core.clj
@@ -2,3 +2,5 @@
 
 (defn empty-board [n]
   (repeat n (repeat n 0)))
+
+(defn spread-mines [board mines-count])
--- test/minesweeper_yellow/core_test.clj
+++ test/minesweeper_yellow/core_test.clj
@@ -4,4 +4,7 @@
 (deftest minesweeper-yellow-tests
   (testing "board creation"
     (testing "given board size n m create board of size n*n"
-      (is (= [[0 0 0] [0 0 0] [0 0 0]] (empty-board 3))))))
+      (is (= [[0 0 0] [0 0 0] [0 0 0]] (empty-board 3))))
+
+    (testing "given board of size n and number of mines m, spread mines on board randomly so that there is m mines at the end"
+      (is (= 3 (filter (fn [e] (= e "b")) (flatten (spread-mines (empty-board 3) 3))))))))
